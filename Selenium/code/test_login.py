0893cc7abc46cd59f9c4e432a2054ebbddbf1f46
Konstantin Ermakov <konstantin.ermakov@corp.mail.ru>
2024-03-28 15:30:03 +0300

    add some code

---
+++ Selenium/code/test_login.py
@@ -0,0 +1,57 @@
+import pytest
+from _pytest.fixtures import FixtureRequest
+
+from ui.pages.base_page import BasePage
+
+
+class BaseCase:
+    authorize = True
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, driver, config, request: FixtureRequest):
+        self.driver = driver
+        self.config = config
+
+        self.login_page = LoginPage(driver)
+        if self.authorize:
+            print('Do something for login')
+
+
+@pytest.fixture(scope='session')
+def credentials():
+        pass
+
+
+@pytest.fixture(scope='session')
+def cookies(credentials, config):
+        pass
+
+
+class LoginPage(BasePage):
+    url = 'https://park.vk.company/'
+
+    def login(self, user, password):
+        return MainPage(self.driver)
+
+
+class MainPage(BasePage):
+    url = 'https://park.vk.company/feed/'
+
+
+class TestLogin(BaseCase):
+    authorize = True
+
+    def test_login(self, credentials):
+        pass
+
+
+class TestLK(BaseCase):
+
+    def test_lk1(self):
+        pass
+
+    def test_lk2(self):
+        pass
+
+    def test_lk3(self):
+        pass
---
+++ Selenium/code/ui/fixtures.py
@@ -0,0 +1,68 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from ui.pages.base_page import BasePage
+from ui.pages.main_page import MainPage
+
+
+@pytest.fixture()
+def driver(config):
+    browser = config['browser']
+    url = config['url']
+    selenoid = config['selenoid']
+    vnc = config['vnc']
+    options = Options()
+    if selenoid:
+        capabilities = {
+            'browserName': 'chrome',
+            'version': '118.0',
+        }
+        if vnc:
+            capabilities['enableVNC'] = True
+        driver = webdriver.Remote(
+            'http://127.0.0.1:4444/wd/hub',
+            options=options,
+            desired_capabilities=capabilities
+        )
+    elif browser == 'chrome':
+        driver = webdriver.Firefox(executable_path='/Users/konstantin.ermakov/Downloads/geckodriver')
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        raise RuntimeError(f'Unsupported browser: "{browser}"')
+    driver.get(url)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def get_driver(browser_name):
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        raise RuntimeError(f'Unsupported browser: "{browser_name}"')
+    browser.maximize_window()
+    return browser
+
+
+@pytest.fixture(scope='session', params=['chrome', 'firefox'])
+def all_drivers(config, request):
+    url = config['url']
+    browser = get_driver(request.param)
+    browser.get(url)
+    yield browser
+    browser.quit()
+
+
+@pytest.fixture
+def base_page(driver):
+    return BasePage(driver=driver)
+
+
+@pytest.fixture
+def main_page(driver):
+    return MainPage(driver=driver)
